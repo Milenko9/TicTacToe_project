e18654aa183d1ba8a1a9014a52c99ceb64915c66
Milenko Stanković <stankovicm1999@gmail.com>
2022-02-01 13:06:43 +0100

    Urajdeno 70%

--- js/main.js
+++ js/main.js
@@ -10,8 +10,10 @@ let result1Div = document.querySelector('.result1');
 let resultTieDiv = document.querySelector('.resultTie');
 let result2Div = document.querySelector('.result2');
 let chooseXODiv = document.querySelector('.choose-x-o');
+// let gameBox = document.querySelectorAll('.xo');
 let pl1;
 let pl2;
+// let symbol = '';
 
 
 chooseX.addEventListener('click', pl1X);
@@ -34,6 +36,7 @@ function pl1O() {
 }
 
 startingGame();
+createTable();
 
 function startingGame() {
   vsCpuBtn.addEventListener('click', playVsCpu);
@@ -68,7 +71,22 @@ function startingGame() {
 
 
 
-createTable();
+var boxes = document.querySelectorAll('.xo');
+var symbol = "O";
+var lines = [
+  [boxes[0],boxes[1],boxes[2]],
+  [boxes[3],boxes[4],boxes[5]],
+  [boxes[6],boxes[7],boxes[8]],
+  [boxes[0],boxes[3],boxes[6]],
+  [boxes[1],boxes[4],boxes[7]],
+  [boxes[2],boxes[5],boxes[8]],
+  [boxes[0],boxes[4],boxes[8]],
+  [boxes[2],boxes[4],boxes[6]]
+];
+
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click',insertSymbol)
+}
 
 function createTable() {
   var text = '';
@@ -78,62 +96,19 @@ function createTable() {
   gameSectionDiv.innerHTML = text;
 }
 
+function insertSymbol() {
+  (symbol == "O")? symbol = "X" : symbol = "O";
+  this.innerHTML = symbol;
+  checkLines();
+}
 
-
-
-
-
-// --------------------------------------------------------------------------------------
-// var vsCpuBtn = document.querySelector('#vsCpu');
-// var introDiv = document.querySelector('.intro');
-// var gameSecConDiv = document.querySelector('.game-section-container');
-// var gameSectionDiv = document.querySelector('.game-section');
-//
-// vsCpuBtn.addEventListener('click', function () {
-//   introDiv.style.display = 'none';
-//   gameSecConDiv.style.display = 'block';
-// })
-//
-// createTable();
-//
-// var boxes = document.querySelectorAll('.xo');
-// var symbol = "O";
-// var lines = [
-//   [boxes[0],boxes[1],boxes[2]],
-//   [boxes[3],boxes[4],boxes[5]],
-//   [boxes[6],boxes[7],boxes[8]],
-//   [boxes[0],boxes[3],boxes[6]],
-//   [boxes[1],boxes[4],boxes[7]],
-//   [boxes[2],boxes[5],boxes[8]],
-//   [boxes[0],boxes[4],boxes[8]],
-//   [boxes[2],boxes[4],boxes[6]]
-// ];
-//
-// for (var i = 0; i < boxes.length; i++) {
-//   boxes[i].addEventListener('click',insertSymbol)
-// }
-//
-// function createTable() {
-//   var text = '';
-//   for (var i = 0; i < 9; i++) {
-//     text += '<div class="xo"></div>';
-//   }
-//   gameSectionDiv.innerHTML = text;
-// }
-//
-// function insertSymbol() {
-//   (symbol == "O")? symbol = "X" : symbol = "O";
-//   this.innerHTML = symbol;
-//   checkLines();
-// }
-//
-// function checkLines() {
-//   for (var i = 0; i < lines.length; i++) {
-//     var line = lines[i];
-//     if (line[0].innerHTML == line[2].innerHTML && line[0].innerHTML == line[1].innerHTML && line[0].innerHTML !== "") {
-//       for (var k = 0; k < line.length; k++) {
-//         line[k].style.background = "tomato";
-//       }
-//     }
-//   }
-// }
+function checkLines() {
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (line[0].innerHTML == line[2].innerHTML && line[0].innerHTML == line[1].innerHTML && line[0].innerHTML !== "") {
+      for (let k = 0; k < line.length; k++) {
+        line[k].style.background = "tomato";
+      }
+    }
+  }
+}
